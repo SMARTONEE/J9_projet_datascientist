d0694b367a7a9dac89e1875abd67e548496dc5d9
SMARTONEE <108334952+SMARTONEE@users.noreply.github.com>
2022-07-08 07:55:47 +0200

    Add files via upload

--- 00_journalists.rb
+++ 00_journalists.rb
@@ -2,30 +2,101 @@
 handles = ["@jcunniet","@PaulLampon","@Aziliz31","@ssoumier","@marionsouzeau","@gaellombart","@bendarag","@AurelieLebelle","@julienduffe","@thomaspoupeau","@LilyRossignol","@ClairGuedon","@stephanieauguy","@claw_prolongeau","@_JulieMenard","@LColcomb","@Zlauwereys","@MeLonguet","@DorotheeLN","@NolwennCosson","@ADaboval","@Remibaldy","@bderveaux","@amandechap","@ELODIESOULIE","@nbongarcon","@HeloAb","@Panamorama","@gregplou","@BenoitBerthe","@LauraBruneau89","@Anthony_Lieures","@Sharonwaj","@mcsonkin","@pverduzier","@emiliel3","@Julien_MARION","@SophiFay","@bdelombre","@annalecerf","@AdriaBudry","@DejNikolic","@iJaffre","@CyrusleVirus","@GPardigon","@e_vallerey","@IsabelleSouquet","@AudeDavidRossi","@Yoann_Pa","@CeliaPenavaire","@perraultvincent","@cboulate","@JustineWeyl","@Paulinejacot","@juliechab","@aslechevallier","@phnou","@Seb_Pommier","@Alex_Bensaid","@GuillaumeGaven","@annelaurechouin","@Oliviader","@guerricp","@JMMarchaut","@cyceron","@gregory_raymond","@vhunsinger","@l_peillon","@fannyguinochet","@EAssayag","@KibweAdom","@YvonHerry","@JohanGarciajg","@saidlabidi","@lauranneprov","@LeaDavy","@francois_remy","@CGuicheteau","@FloMaillet","@m_perroud","@oBrunet_TSMF","@MoonVdc","@jc2taille","@NellyMoussu","@VirginK","@b_misa","@FabriceCouste","@barbara_prose","@lelia2m","@brunoaskenazi","@laurenechamp","@ysisbox","@juliengagliardi","@PierreLel","@kdeniau","@_TerraInc","@DominicArpin","@antoinfonteneau","@nanotousch","@jb_roch","@YaniKhezzar","@Anne_Bechet","@NCapart","@SamyBenNaceur","@Joumany","@Julietteraynal","@TGiraudet","@SaraTanit","@HappeFrederic","@antoinellorca","@michelpicot","@Sev_Ryne","@bobdobolino","@murdever","@YGrandmontagne","@Mnyo","@EdKOSCIANSKI","@tnoisette","@jankari","@delbello_rom","@rflechaux","@NadiaSorelli","@IT_Digital","@abarbaux","@PhilippeLeroy","@schaptal","@marionspee","@lisavignoli","@ChloeAeberhardt","@MartineJacot","@JuliaPascualita","@curieusedetout","@sgraveleau","@bif_o","@ElisaPineau","@zinebdryef","@apiquard","@pierrehaski","@StephanieDelmas","@Blandine_Garot","@vergara_i","@evan_lebastard","@SophieVclt","@OlivierLevrault","@alicedorgeval","@LouiseMalnoy","@alix_fx","@pierre_baudis","@LucMagoutier","@AgatheMuller","@SGianninelli","@PaulineBoyer33","@NaomiHalll","@romaindlx","@marionbr","@Burtschy","@JacobEtienne","@as_lizzani","@marie_simon","@LaureDaussy","@FabriceAmedeo","@min","@LoubnaChlaikhy","@PlummerWilliam","@OlivierMarin1","@alaurefremont","@mwesfreid","@ChBaDe","@pmathon","@theobaldmarie","@Lnpagesy","@marclandre","@paoliniesther","@Feertchak","@JBLitzler","@GuillaumeErrard","@quentinperinel","@TristanQM","@mlbo","@constancejamet","@LoraTerrazas","@emiliegeffray","@Mathilde_Sd","@CaroPiquet","@DCanivez","@TIM_7375","@blandinelc","@ivanrioufol","@arthurberdah","@SarahLecoeuvre","@guillaume_gui","@DamienMercereau","@W_Chloe","@Assma_MD","@EugenieBastie","@HiTech_lexpress","@bcondominas","@Laurie_Z_","@jeanfrancgerard","@MathieuPagura","@BGUYF","@AlainPiffaretti","@AudreyKucinskas","@julienhory","@Pierrefalga","@TiphThuillier","@cdaniez","@LigerBaptiste","@D_Peras","@julie_dlb","@Fatiha_Temmouri","@julian2lamancha","@GaetaneDeljurie","@JulianMattei","@M_Vicuna","@DeBruynOlivier","@Nehed_Jendoubi","@antoine_grenapi","@ColonnaGen","@VictoriaGairin","@Clement_Lacombe","@TVigoureux","@MargauxObriot","@solinedelos","@RocheSabine","@dangerkens","@EdouardDutour","@MDondeyne","@DupuisNathalie1","@bouscarel","@Mathieu2jean","@Sophie_T_J","@laurentcalixte","@patrockwilliams","@PascaleKremer","@AlexJaquin","@LauraIsaaz","@cath_robin","@Del_Gautherin","@Isaduriez","@lucietuile","@AugeyBastien","@mcastagnet","@AminaKalache","@mvaudano","@CParrot","@ombelinetips","@_JoinLion","@BarbolosiRose","@ToiBruno1","@FloraClodic","@xjbdx","@ceci_est_un_handle_vraiment_long","@AlexiaEy","@Emjy_STARK","@elcoco01","@rabilebon","@pflovens_","@FabriceFrossard","@MorganeSERRES","@MarjolaineKoch","@edgarsnow","@SRNLF","@CChassigneux","@KerinecMoran","@NassiraELM","@NewsRicard","@Sandreene","@Emilezrt","@Pierre_Do","@Micode","@CColumelli","@DavidAbiker","@ClementBergantz","@benjaminrabier","@celinekallmann","@edwyplenel","@C_Barbier","@JJBourdin_RMC","@LaurenceFerrari","@aslapix","@IsaMillet","@MaximeSwitek","@tomjoubert","@jszanchi","@roqueeva","@XavierBiseul","@florencesantrot","@AntoineCrochet","@freeman59","@MaudeML","@philippe_gulpi","@mathieum76","@kiouari","@imanemoustakir","@BenedicteMallet","@Emilie_Brouze","@antoinebarret","@_nicolasbocquet","@remibuhagiar","@CourretB","@AymericRobert","@miraelmartins","@pmaniere","@jesuisraphk","@David_Ingram","@pcelerier","@technomedia","@Geraldinedauver","@ThierryLabro","@Newsdusud","@nrauline","@gbregeras","@SCouasnonBFM","@actualites_nrv","@dimitrimoulins","@oli_aura","@FabieChiche","@Vincent_Raimblt","@ChristophGreuet","@PAlbuchay","@MarrauddesGrot","@vtalmon","@cedric","@olivierfrigara","@Julien_Jay","@LydiaBerroyer","@Shuua","@datisdaz","@Steuph","@ameliecharnay","@Bruno_LesNums","@LelloucheNico","@CciliaDiQuinzio","@Elodie_C","@SylvRolland","@Kocobe","@FL_Debes","@jdupontcalbo","@GarciaVictor_","@NicoRichaud","@RHoueix","@simottel","@DamienLicata","@annabelle_L","@Lea_Lejeune","@axel_deb","@marin_eben","@ptiberry","@MatthieuDelach","@sandrinecassini","@benjaminferran","@ppgarcia75","@NotPatrick","@ivalerio","@FabienneSchmitt","@alexgoude","@JeromeColombain","@manhack","@Capucine_Cousin","@Fsorel","@oliviertesquet","@marjoriepaillon","@ginades","@PierreTran","@DelphineCuny","@reesmarc","@lauratenoudji","@ldupin","@carolinedescham","@Lucile_Quillet","@cgabizon","@Allocab","@epenser","@JAGventeprivee","@frwrds","@Laure__Bourdon","@Xavier75","@maximeverner","@s_jourdain","@LoriHelloc"]
 
 
-#Combien y a t'il de personne dans cette array ? 
-puts "il y a #{handles.length} dans ce tableau"
+# #Combien y a t'il de personne dans cette array ? 
+def nb_hdls(handles)
+    puts "il y a #{handles.length} dans ce tableau"
+end
+
+
 
 # Quel est le handle le plus court ? 
-
-
-
-#Combien de handle contenant 5 caractères (@ exclue) faire un if length = 5 mettre dans un array puis calculer la size d'un handle
-# puts "Il y a #{handles.count {|x| x.length = 6}} avec 5 caractères"
-
-
+def smallest_handle(handles)
+    puts "Le handle le plus court de la liste est #{handles.min {|a, b| a.length <=> b.length }}"
+end
 
 
 
 
-#Combien commencent par une Majuscule ? 
+# #Combien de handle contenant 5 caractères (@ exclue) faire un if length = 5 mettre dans un array puis calculer la size d'un handle
+def big_cara_handles(handles)
+    puts "Il y a #{handles.count {|x| x.length == 6}} avec 5 caractères"
+end
+
+
+
+
+# #Combien commencent par une Majuscule ? 
+def maj_handles(handles)
+    puts "to do"
+end
+
+
 #Trie la liste de handle par ordre alpa
+def alpha_handles(handles)
+    puts "la liste par ordre alpha : #{handles.sort}"
+end
 
-puts "la liste par ordre alpha : #{handles.sort}"
+
+# #Trie la liste par taille des handles (size ou length) check diff entre size et length
+
+def size_handles(handles)
+    puts "from the mas pequeno to the biggest option 1: #{handles.sort_by {|x| x.size}}"
+end
+# puts "from the mas pequeno to the biggest option 2: #{handles.sort_by {|x| x.length}}"
+
+# #QUelle est la position dans le handle de la personne @epenser
+def position_handles(handles)
+    puts "La personne @epenser est le numéro #{handles.index {|x| x == "@epenser"}}"
+end
 
 
-#QUelle est la position dans le handle de la personne @epenser
+# #Répartition par taille des handles
+#1 trouver le plus grand handle
+#2 créer une boucle selon le plus grand handle : tant que x n'a pas atteint le plus grand handle, répéter
 
-puts "La personne @epenser est le numéro #{handles.index? @epenser}"
+def max_handles(handles)
+    puts "la plus grande valeur de cette liste est #{handles.max {|a, b| a.length <=> b.length }}, elle va nous permettre de créer une boucle qui tend vers cette valeur"
 
-#Répartition par taille des handles
+    nbcara = 0
+
+    while nbcara <= 35
+        puts "Il y a dans cette liste #{handles.count {|x| x.length == nbcara}} handle d'une taille de #{nbcara} caractère"
+        nbcara += 1
+    end
+end
+
+
+puts "what do you want to know ?
+    1 : 
+    2 : 
+    3 :
+    4 : 
+    5 :
+    6 :
+    7 : 
+    8 : "
+number = gets.to_i 
+
+    if number == 1
+        puts nb_hdls(handles)
+        elsif number == 2
+            puts smallest_handle(handles)
+        elsif number == 3
+            puts big_cara_handles(handles)
+        elsif number == 4
+            puts maj_handles(handles)
+        elsif number ==5
+            puts alpha_handles(handles)
+        elsif number == 6
+            puts size_handles(handles)
+        elsif number == 7
+            puts position_handles(handles)
+        elsif nuber == 8 
+            puts max_handles(handles)
+    end
+
+
+
+
+
 
